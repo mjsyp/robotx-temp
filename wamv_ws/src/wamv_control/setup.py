a6e478aa2af6b5608f5b55f419b987ef840f0ee1
Michael Sypniewski <mjsyp@umich.edu>
2018-10-21 23:37:58 -0400

    more

--- wamv_ws/src/wamv_control/setup.py
+++ wamv_ws/src/wamv_control/setup.py
@@ -5,8 +5,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['teleop_controller'],
-    package_dir={'': 'nodes'},
+    packages=['wamv_control'],
+    package_dir={'': 'src'},
 )
 
 setup(**setup_args)
